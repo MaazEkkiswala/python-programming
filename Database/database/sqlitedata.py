79f21f4353f33733656013405f54d80d8f3a090e
Maaz Ekkiswala <93048323+MaazEkkiswala@users.noreply.github.com>
2022-01-06 11:55:24 +0530

    Add files via upload

--- Database/database/sqlitedata.py
+++ Database/database/sqlitedata.py
@@ -4,7 +4,8 @@ a=sqlite3.connect('newdata.db')
 a.execute('''CREATE TABLE IF NOT EXISTS STUDENT(NAME TEXT,AGE INT)''')
 print("table created")
 
-a.execute("update into student where name='xyz' set('abc',85)")
+a.execute("update student set name='abc' where name='xyz'")
+print("updated successfully")
 
 a.execute("insert into student(name,age) values('xyz',56)")
 
